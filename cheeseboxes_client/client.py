8eb224bb6a512edfd227a94e27caceda8d164045
Daniel Eberhard <daniel.h.e@gmx.de>
2020-05-03 03:04:11 +0200

    change version

--- cheeseboxes_client/client.py
+++ cheeseboxes_client/client.py
@@ -3,10 +3,10 @@ import socket
 from game import game
 from box import box
 
-version = "0.2.0.0"
+version = "0.2.0.1"
 
 HOST = '193.31.24.180'  # The server's hostname or IP address
-HOST = '127.0.0.1'  # The server's hostname or IP address
+#HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 gamePlay = None
